f4a14e1b6d95547e3e417c91c4f2234d49eb6609
ghazalkeshavarzi <ghazal.keshavarzi7@gmail.com>
2021-07-05 21:33:57 +0430

    worked on javascript on localstorage!

--- js/app.js
+++ js/app.js
@@ -1,19 +1,46 @@
-//variables
+//VARIABLES
+const doList=document.querySelector('#result');
 
 
 
-
-//eventlisteners
-
+//EVENTLISTENERS
 eventListeners();
-function eventListeners(){
-    document.querySelector('#btn').addEventListener('submit',addNote);
+function eventListeners() {
+    //submission
+    document.querySelector('#btn1').addEventListener('click',addNote);
+
+    //delete note
+    document.querySelector('#dolist').addEventListener('click',deleteNote);
 }
 
 
 
-//functions
 
+//FUNCTIONS
+
+//add note to the list
 function addNote(e) {
-    e.preventDefault()
+    e.preventDefault();
+    const note=document.querySelector('#notebook1').value;
+
+    // create remove buttton
+    const remove=document.createElement("a");
+    remove.textContent='X';
+    remove.classList='remove_btn';
+    console.log(remove);
+
+    // create <li> tag
+    const li=document.createElement('li');
+    li.appendChild(document.createTextNode(note));
+    li.appendChild(remove);
+
+    //add the lis to the do list
+    doList.appendChild(li);
+
+
+}
+function deleteNote(e) {
+    if (e.target.classList.contains('remove_btn')) {
+        e.target.parentElement.remove();
+    }
 }
